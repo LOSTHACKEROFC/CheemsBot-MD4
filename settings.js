bf4f0247a4127cfd5e2b9d50cbe2d8afd971863c
LOST HACKER <96609972+LOSTHACKEROFC@users.noreply.github.com>
2022-07-07 22:36:44 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -37,9 +37,9 @@ global.APIKeys = {
 
 //aumto functioner
 global.autoTyping = false //auto tying in gc (true to on, false to off)
-global.autoreadpmngc = false //auto reading in gc and pm (true to on, false to off)
-global.autoReadGc = false //auto reading in gc (true to on, false to off)
-global.autoRecord = false //auto recording (true to on, false to off)
+global.autoreadpmngc = true //auto reading in gc and pm (true to on, false to off)
+global.autoReadGc = true //auto reading in gc (true to on, false to off)
+global.autoRecord = true //auto recording (true to on, false to off)
 global.available = true //auto available (true to on, false to off)
 
 //semt menu variants
@@ -54,23 +54,23 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.vcardowner = ['916909137213'] //ur owner number
-global.ownername = "🦄Dream Guy Xeon" //ur owner name
-global.ytname = "YT: Xeon" //ur yt chanel name
-global.socialm = "GitHub: DGXeon" //ur github or insta name
-global.location = "India, Mizoram, Aizawl" //ur location
+global.vcardowner = ['919536476115'] //ur owner number
+global.ownername = "👨🏻‍💻CHIRAG YOUTUBER 👨🏻‍💻" //ur owner name
+global.ytname = "CHIRAG YOUTUBER" //ur yt chanel name
+global.socialm = "github.com/LOSTHACKEROFC" //ur github or insta name
+global.location = "India, UTTARAKHAND , DEV BHUMI" //ur location
 
 //bot bomdy 
-global.owner = ['916909137213']
-global.ownertag = '916909137213' //ur tag number
-global.botname = 'Cheems Bot MD V4' //ur bot name
-global.linkz = "https://youtu.be/WiIqCdiDjFo" //your theme url which will be displayed on whatsapp
-global.websitex = "https://youtu.be/WiIqCdiDjFo" //ur website to be displayed
-global.botscript = 'https://github.com/DGXeon/CheemsBot-MD4' //script link
-global.reactmoji = "😘" //ur menu react emoji
-global.themeemoji = "🦄" //ur theme emoji
-global.packname = "Cheems Bot Stickers\n\n\n\n\n\n\n\n\n\n\n🦄🦄🦄 " //ur sticker watermark packname
-global.author = "🦄Dream Guy Xeon" //ur sticker watermark author
+global.owner = ['919536476115']
+global.ownertag = '919536476115' //ur tag number
+global.botname = '🤡𝘾𝙃𝙄𝙍𝘼𝙂 𝙔𝙊𝙐𝙏𝙐𝘽𝙀𝙍🤡' //ur bot name
+global.linkz = "https://www.youtube.com/channel/UCOYVQIN4SGwaKV2ukY5FgdA" //your theme url which will be displayed on whatsapp
+global.websitex = "https://chirag-youtuber-chirag.on.drv.tw/site" //ur website to be displayed
+global.botscript = 'https://github.com/LOSTHACKEROFC' //script link
+global.reactmoji = "🤡" //ur menu react emoji
+global.themeemoji = "💋" //ur theme emoji
+global.packname = "SUBSCRIBE\n\n\n\n\n\n\n\n\n\n\n𝘾𝙃𝙄𝙍𝘼𝙂 𝙔𝙊𝙐𝙏𝙐𝘽𝙀𝙍😈 " //ur sticker watermark packname
+global.author = "CHIRAG YOUTUBER 🥵" //ur sticker watermark author
 
 //Bot theme media
 global.thum = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur thumb pic
@@ -79,7 +79,7 @@ global.err4r = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur error pic
 global.thumb = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur thumb pic
 
 //damtabase
-global.premium = ['916909137213'] //ur premium numbers
+global.premium = ['919536476115'] //ur premium numbers
 global.ntilinkytvid = []
 global.ntilinkytch = []
 global.ntilinkig = []
